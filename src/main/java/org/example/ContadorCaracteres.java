4189d682ac4e383579f1ea92ccb2859443eda9ed
RAQUEL DA SILVA <raquel_silva10@estudante.sesisenai.org.br>
2025-09-17 17:41:18 -0300

    questao caracteres

---
+++ src/main/java/org/example/ContadorCaracteres.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class ContadorCaracteres {
+
+    public int contar(String texto, char letrar){
+
+
+        for(int i=0; i< texto.length(); i++){
+            if
+        }
+    }
+}
